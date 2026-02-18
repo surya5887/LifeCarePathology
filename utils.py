ac6fcb9616e08ad636cb6926810cc2d717de6bdd
Anees Chaudhary <samirchaudhary8558@gmail.com>
2026-02-18 11:11:57 +0530

    Fix unauthorized error: Redirect to login instead of 401

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 from functools import wraps
-from flask import abort
+from flask import abort, redirect, url_for, request, flash
 from flask_login import current_user
 
 
@@ -14,7 +14,8 @@ def role_required(role):
         @wraps(f)
         def wrapped(*args, **kwargs):
             if not current_user.is_authenticated:
-                abort(401)
+                flash('Please login to access this page.', 'info')
+                return redirect(url_for('auth.login', next=request.url))
 
             if current_user.role != role:
                 abort(403)
